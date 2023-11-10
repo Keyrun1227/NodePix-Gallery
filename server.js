6c1cc46ab1adb02ac1529901227225c9489518f0
Sai Kiran <102981731+Keyrun1227@users.noreply.github.com>
2023-11-11 04:09:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,9 +28,9 @@ const User = mongoose.model('User', {
 
 // Passport setup
 passport.use(new GoogleStrategy({
-    clientID: '1060550103437-5090vgarge892n1cji57m4v3e916krsb.apps.googleusercontent.com',
-    clientSecret: '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',
-    callbackURL: 'http://localhost:3000/auth/google/callback',
+  clientID: 'YOUR_CLIENT_ID',
+  clientSecret: 'YOUR_CLIENT_SECRET',
+  callbackURL: 'http://localhost:3000/auth/google/callback',
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     // Check if the user already exists
